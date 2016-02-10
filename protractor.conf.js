96611b58bbf388fc3f628a6329f40160b0854cd0
Feodor Fitsner <feodor@appveyor.com>
2016-02-09 18:00:32 -0800

    Do not use BrowserStack

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,21 +1,6 @@
-﻿exports.config = {
-    seleniumAddress: 'http://hub.browserstack.com/wd/hub',
-
-    capabilities: {
-        'browserstack.user': process.env.BROWSERSTACK_USER,
-        'browserstack.key': process.env.BROWSERSTACK_KEY,
-
-        // Needed for testing localhost
-        'browserstack.local': 'true',
-
-        // Settings for the browser you want to test
-        // (check docs for difference between `browser` and `browserName`
-        'browserName': 'Chrome',
-        'browser_version': '46.0',
-        'os': 'Windows',
-        'os_version': '7',
-        'resolution': '1280x1024'
-    },
+exports.config = {
+    // path to the selenium server jar. Update version number accordingly!
+    seleniumServerJar: 'node_modules/protractor/selenium/selenium-server-standalone-2.48.2.jar',
 
     // select all end to end tests
     specs: ['AppVeyor-Protractor-test-app/*e2e.spec.js'],
