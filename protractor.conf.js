587a95923349a519f9b57cfdf4c70c1198b27951
Feodor Fitsner <feodor@appveyor.com>
2016-02-09 18:07:11 -0800

    Correct path to Selenium jar

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,6 +1,6 @@
 exports.config = {
     // path to the selenium server jar. Update version number accordingly!
-    seleniumServerJar: 'node_modules/protractor/selenium/selenium-server-standalone-2.48.2.jar',
+    seleniumServerJar: 'node_modules/protractor/selenium/selenium-server-standalone-2.51.0.jar',
 
     // select all end to end tests
     specs: ['AppVeyor-Protractor-test-app/*e2e.spec.js'],
