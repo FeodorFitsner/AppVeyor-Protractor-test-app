ca54bf0b068cc7217802ff1785b29889cc600adb
Feodor Fitsner <feodor@appveyor.com>
2016-02-09 18:09:20 -0800

    Add selenium args

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,6 +1,10 @@
 exports.config = {
     // path to the selenium server jar. Update version number accordingly!
     seleniumServerJar: 'node_modules/protractor/selenium/selenium-server-standalone-2.51.0.jar',
+    
+    localSeleniumStandaloneOpts: {
+        args: ['-Djna.nosys=true']
+    },
 
     // select all end to end tests
     specs: ['AppVeyor-Protractor-test-app/*e2e.spec.js'],
