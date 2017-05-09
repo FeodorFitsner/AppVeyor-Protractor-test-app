f0adaac2445e3a05e215129af1dd99a0009edbcf
Feodor Fitsner <feodor@appveyor.com>
2017-05-09 14:25:13 -0700

    jar comment [skip ci]

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,6 +1,6 @@
 exports.config = {
     // path to the selenium server jar. Update version number accordingly!
-    seleniumServerJar: 'node_modules/protractor/selenium/selenium-server-standalone-2.51.0.jar',
+    //seleniumServerJar: 'node_modules/protractor/selenium/selenium-server-standalone-2.51.0.jar',
     
     localSeleniumStandaloneOpts: {
         args: ['-Djna.nosys=true']
